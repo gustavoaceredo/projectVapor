f2a1fac12c0e685d8a08d6802b9e4acbf1d0ee7f
Gustavo Aceredo <gustavo.aceredo@gmail.com>
2018-05-02 21:14:35 -0500

    subo mi primer ejemplo con vapor

---
+++ Sources/App/Controllers/UserRegistration.swift
@@ -0,0 +1,8 @@
+//
+//  UserRegistration.swift
+//  App
+//
+//  Created by VECTOR on 5/1/18.
+//
+
+import Foundation
---
+++ Sources/App/Controllers/allUser.swift
@@ -0,0 +1,8 @@
+//
+//  allUser.swift
+//  App
+//
+//  Created by VECTOR on 5/2/18.
+//
+
+import Foundation
